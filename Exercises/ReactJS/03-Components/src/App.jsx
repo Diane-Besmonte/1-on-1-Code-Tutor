222019a3b4195ef96e6b26f81e980745325aa3d5
Diane <elsiebesmonte@gmail.com>
2024-04-17 00:28:16 +0800

    Done with Exercises 3 and 4

--- Exercises/ReactJS/03-Components/src/App.jsx
+++ Exercises/ReactJS/03-Components/src/App.jsx
@@ -1,37 +1,70 @@
 // CREATE YOUR ContactCard COMPONENT HERE
-
+const ContactCard = ({ name, position, email }) => {
+  return (
+    <>
+      <ul>
+        <li className="contact-card">
+          <h2>{name}</h2>
+          <dl>
+            <dt>Job</dt>
+            <dd>{position}</dd>
+            <dt>Email</dt>
+            <dd>{email}</dd>
+          </dl>
+        </li>
+      </ul>
+    </>
+  );
+};
 function App() {
   return (
     // REPLACE THIS WITH COMPONENTS
-    <ul>
-      <li className="contact-card">
-        <h2>John Doe</h2>
-        <dl>
-          <dt>Job</dt>
-          <dd>Electrical Engineer</dd>
-          <dt>Email</dt>
-          <dd>john.doe@acme.co</dd>
-        </dl>
-      </li>
-      <li className="contact-card">
-        <h2>Pedro Penduko II</h2>
-        <dl>
-          <dt>Job</dt>
-          <dd>Receptionist</dd>
-          <dt>Email</dt>
-          <dd>ppenduko@acme.co</dd>
-        </dl>
-      </li>
-      <li className="contact-card">
-        <h2>Billy Boy Magnaye</h2>
-        <dl>
-          <dt>Job</dt>
-          <dd>President</dd>
-          <dt>Email</dt>
-          <dd>bbm@acme.co</dd>
-        </dl>
-      </li>
-    </ul>
+    // <ul>
+    //   <li className="contact-card">
+    //     <h2>John Doe</h2>
+    //     <dl>
+    //       <dt>Job</dt>
+    //       <dd>Electrical Engineer</dd>
+    //       <dt>Email</dt>
+    //       <dd>john.doe@acme.co</dd>
+    //     </dl>
+    //   </li>
+    //   <li className="contact-card">
+    //     <h2>Pedro Penduko II</h2>
+    //     <dl>
+    //       <dt>Job</dt>
+    //       <dd>Receptionist</dd>
+    //       <dt>Email</dt>
+    //       <dd>ppenduko@acme.co</dd>
+    //     </dl>
+    //   </li>
+    //   <li className="contact-card">
+    //     <h2>Billy Boy Magnaye</h2>
+    //     <dl>
+    //       <dt>Job</dt>
+    //       <dd>President</dd>
+    //       <dt>Email</dt>
+    //       <dd>bbm@acme.co</dd>
+    //     </dl>
+    //   </li>
+    // </ul>
+    <>
+      <ContactCard
+        name="John Doe"
+        position="Electrical Engineer"
+        email="john.doe@acme.co"
+      />
+      <ContactCard
+        name="Pedro Penduko II"
+        position="Receptionist"
+        email="ppenduko@acme.co"
+      />
+      <ContactCard
+        name="Billy Boy Magnaye"
+        position="President"
+        email="bbm@acme.co"
+      />
+    </>
   );
 }
 
--- Exercises/ReactJS/04-Children-props/src/App.jsx
+++ Exercises/ReactJS/04-Children-props/src/App.jsx
@@ -1,36 +1,69 @@
 // PUT YOUR COMPONENT HERE OR MAKE A NEW COMPONENT FILE AND IMPORT HERE
+const NewButton = ({ bordercolor, textcolor, backgroundcolor, content }) => {
+  return (
+    <>
+      <button
+        style={{
+          border: "2px solid",
+          color: textcolor,
+          borderColor: bordercolor,
+          background: backgroundcolor,
+          borderRadius: 4,
+          padding: 16,
+          margin: 8,
+        }}
+      >
+        {content}
+      </button>
+    </>
+  );
+};
 
 function App() {
   // REPLACE THIS WITH COMPONENTS
   return (
-    <div>
-      <button
-        style={{
-          border: "2px solid",
-          color: "white",
-          borderColor: "red",
-          background: "red",
-          borderRadius: 4,
-          padding: 16,
-          margin: 8,
-        }}
-      >
-        Cancel
-      </button>
-      <button
-        style={{
-          border: "2px solid",
-          color: "black",
-          borderColor: "skyblue",
-          background: "skyblue",
-          borderRadius: 4,
-          padding: 16,
-          margin: 8,
-        }}
-      >
-        Confirm
-      </button>
-    </div>
+    // <div>
+    //   <button
+    //     style={{
+    //       border: "2px solid",
+    //       color: "white",
+    //       borderColor: "red",
+    //       background: "red",
+    //       borderRadius: 4,
+    //       padding: 16,
+    //       margin: 8,
+    //     }}
+    //   >
+    //     Cancel
+    //   </button>
+    //   <button
+    //     style={{
+    //       border: "2px solid",
+    //       color: "black",
+    //       borderColor: "skyblue",
+    //       background: "skyblue",
+    //       borderRadius: 4,
+    //       padding: 16,
+    //       margin: 8,
+    //     }}
+    //   >
+    //     Confirm
+    //   </button>
+    // </div>
+    <>
+      <NewButton
+        bordercolor="red"
+        textcolor="white"
+        backgroundcolor="red"
+        content="Cancel"
+      />
+      <NewButton
+        bordercolor="skyblue"
+        textcolor="black"
+        backgroundcolor="skyblue"
+        content="Confirm"
+      />
+    </>
   );
 }
 
